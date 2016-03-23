fe51fafb0adb8a83f9ed93ec99d16a278ac85031
anyjava <sht21c@gmail.com>
2016-03-23 14:35:37 +0900

    JPA 257page 예제 실습 - TypQuery 실습

--- src/main/java/jpabook/start/JpaMain.java
+++ src/main/java/jpabook/start/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import java.util.Collection;
 import java.util.List;
 
@@ -19,7 +20,6 @@ public class JpaMain {
         try {
             tx.begin();
             logic(em);
-//            logic2(em);
             tx.commit();
 
         } catch (Exception e) {
@@ -30,43 +30,6 @@ public class JpaMain {
         }
     }
 
-    private static void logic2(EntityManager em) {
-
-        Board board = new Board();
-        board.setId("id1");
-        board.setTitle("테스트");
-        board.setContent("dsafasdfsad");
-        board.setViewCount(10);
-
-        em.persist(board);
-
-        board.setContent("modified content");
-        board.setTitle("modified Title");
-
-
-        Board findBoard = em.find(Board.class, board.getId());
-        System.out.println("findBoard = " + findBoard);
-
-
-        String findStr = "ed";
-        List<Board> resultList
-                = em.createQuery(
-                        "select b " +
-                        "from Board b " +
-                        "where b.title like :search", Board.class)
-                .setParameter("search", "%" + findStr + "%")
-                .getResultList();
-
-        System.out.println("resultList = " + resultList);
-
-        for (Board b : resultList) {
-            System.out.println("board = " + b);
-        }
-
-        em.remove(board);
-
-        Collection c;
-    }
 
     private static void logic(EntityManager em) {
 
@@ -82,18 +45,12 @@ public class JpaMain {
         em.persist(team);
 
 
+        TypedQuery<Member> query = em.createQuery("SELECT m FROM Member m where m.age > 10", Member.class);
+        List<Member> resultList = query.getResultList();
 
-
-
-        Team teamB = new Team();
-        teamB.setName("TeamB");
-
-        em.persist(teamB);
-
-        // 단건조회
-        Member findMember = em.find(Member.class, member.getId());
-        System.out.println("findMember = " + findMember);
-
+        for (Member member1 : resultList) {
+            System.out.println("member1 = " + member1);
+        }
 
     }
 }
